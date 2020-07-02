f53e31be9ffb4a76e9815ef51ea639425dd7f64c
Kirk Patton <kpatton@verizonmedia.com>
2020-07-02 12:03:43 -0700

    mergo v.0.3.9 returns and err on merge of differet types

--- main.go
+++ main.go
@@ -9,46 +9,17 @@ import (
 type Foo map[interface{}]interface{}
 
 func main() {
-	// 3.9 error on different types to merge
-	// interesting 3.8 merged maxMemory where one was a string and the other was an int
-	config := `
----
-image: docker.ouroath.com:4443/yahoo-cloud/redis-docker:20200228-202415
-appName: systemregistry-redis
-athensDomain: stat.k8s
-masters: 2
-replicas: 1
-ykkName: systemregistry-redis-auth
-ykkGroup: systemregistry.development
-ykkEnv: aws
-slaveServeStaleData: "no"
-maxMemory: "5Gi"
-port: 8021
-cpu: "1.5"
-environmentVariables:
-  yamasNamespace: stat
-jobs:
-  component:
-    colo: corp.bf1
-    environment: development
-  prod-corp-bf1:
-    colo: corp.bf1
-    environment: development
-`
+	// v0.3.9: error on different types to merge
+	// cannot append two different types (int, string)
 
-	defaultConfig := `
-replicas: 0
-port: 6379
-maxMemory:  3221225472
-maxMemoryPolicy: "allkeys-lru"
-cpu: "500m"
-timeoutSeconds: 60
-clusterNodeTimeoutMillis: 5000
-save: ""
-slaveServeStaleData: "yes"
-ykkEnv: "prod"
-maxclients: 10000
-`
+	// v0.3.8: and earlier allow this
+	//
+	// There is a mergo.WithTypeCheck(c *Config)
+	// but that defaults to false, so this behavior is
+	// unexpected
+
+	config := `foo: "1"`
+	defaultConfig := `foo: 1`
 
 	var (
 		configMap  map[interface{}]interface{}
@@ -65,6 +36,8 @@ maxclients: 10000
 		panic(err)
 	}
 
+	// v0.3.9 returns an error
+	// versions <= v0.3.8 do not
 	err = mergo.Merge(&configMap, &defaultMap)
 	if err != nil {
 		panic(err)
